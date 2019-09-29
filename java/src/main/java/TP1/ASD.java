cbb954999dee697bfead89f8201208e3a331c8af
emk <emk@Naklabik>
2019-09-29 22:09:40 +0100

    Partie2.1

--- java/src/main/java/TP1/ASD.java
+++ java/src/main/java/TP1/ASD.java
@@ -1,13 +1,38 @@
 package TP1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ASD {
-  static public class Document {
-    // Fill here!
-    public String toNtriples() {
-      return "Manu est dans la place   !";
-    } 
-  }
-  // Fill here!
+	static public class Document {
+
+		private List<Phrase> phrases = new ArrayList<>();
+
+		public Document(Phrase phrase) {
+			this.phrases.add(phrase);
+		}
+
+		public Document(List<Phrase> phrase) {
+			this.phrases = phrase;
+		}
+
+		public String toNtriples() {
+			String Ntriples = "";
+			for (Phrase p : this.phrases) {
+				for (Reste r : p.getRestes()) {
+					for (Objet o : r.getObjects()) {
+						Ntriples = Ntriples + " " +
+								p.getSujet().getName() + " " + 
+								r.getPredicat().getName() + " "+
+								o.getName() +" .\n";
+
+					}
+				}
+
+			}
+
+			return Ntriples;
+		}
+	}
+	// Fill here!
 }
--- java/src/main/java/TP1/Main.java
+++ java/src/main/java/TP1/Main.java
@@ -1,6 +1,8 @@
 package TP1;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 
 /*
@@ -10,41 +12,77 @@ import org.antlr.v4.runtime.CommonTokenStream;
 */
 
 public class Main {
-  public static void main(String[] args) {
-    // Use with a manually made AST
-    ASD.Document ast = new ASD.Document(/* Fill here! */);
-    System.out.println(ast.toNtriples());
+	public static void main(String[] args) {
 
+		
+		List<Phrase> phrases = new ArrayList<>();
 
-    // Use with lexer and parser
-    /*
-    try {
-      // Set input
-      CharStream input;
-      if(args.length == 0) {
-        // From standard input
-        input = CharStreams.fromStream(System.in);
-      }
-      else {
-        // From file set in first argument
-        input = CharStreams.fromPath(Paths.get(args[0]));
-      }
+		List<Reste> r1 = new ArrayList<>();
+		List<Reste> r2 = new ArrayList<>();
+		
+		Sujet s1 = new Sujet("poly117");
+		Sujet s2 = new Sujet("Ridoux");
+		
+		Objet poly = new Entite("poly");
+		Objet ridoux = new Entite("Ridoux");
+		Objet ferre = new Entite("Ferre");
+		Objet compilation = new Text("Compilation");
+		Objet personne = new Entite("personne");
+		Objet professeur = new Entite("professeur");
+		
+		List<Objet> autors = new ArrayList<>();
+		List<Objet> typeList2 = new ArrayList<>();
+		
 
-      // Instantiate Lexer
-      TurtleLexer lexer = new TurtleLexer(input);
-      CommonTokenStream tokens = new CommonTokenStream(lexer);
+		Predicat p1 = new Predicat("type");
+		Predicat p2 = new Predicat("auteur");
+		Predicat p3 = new Predicat("titre");
+		
+		autors.add(ridoux);
+		autors.add(ferre);
+		
+		typeList2.add(personne);
+		typeList2.add(professeur);
 
-      // Instantiate Parser
-      TurtleParser parser = new TurtleParser(tokens);
+		
+		Reste type1 = new Reste(p1, poly);
+		Reste auteur = new Reste(p2, autors);
+		Reste titre = new Reste(p3, compilation);
+		
+		r1.add(type1);
+		r1.add(auteur);
+		r1.add(titre);
+		
+		
+		Reste type2 = new Reste(p1, typeList2);
+		
+		r2.add(type2);
+		
+		Phrase ph1 = new Phrase(s1, r1);
+		Phrase ph2 = new Phrase(s2, r2);
 
-      // Parse
-      ASD.Document ast = parser.document().out;
+		phrases.add(ph1);
+		phrases.add(ph2);
 
-      // Print as Ntriples
-      System.out.println(ast.toNtriples());
-    } catch(IOException e) {
-      e.printStackTrace();
-    }
-    */
-  }
+		// Use with a manually made AST
+		ASD.Document ast = new ASD.Document(phrases);
+		System.out.println(ast.toNtriples());
+
+		// Use with lexer and parser
+		/*
+		 * try { // Set input CharStream input; if(args.length == 0) { // From standard
+		 * input input = CharStreams.fromStream(System.in); } else { // From file set in
+		 * first argument input = CharStreams.fromPath(Paths.get(args[0])); }
+		 * 
+		 * // Instantiate Lexer TurtleLexer lexer = new TurtleLexer(input);
+		 * CommonTokenStream tokens = new CommonTokenStream(lexer);
+		 * 
+		 * // Instantiate Parser TurtleParser parser = new TurtleParser(tokens);
+		 * 
+		 * // Parse ASD.Document ast = parser.document().out;
+		 * 
+		 * // Print as Ntriples System.out.println(ast.toNtriples()); }
+		 * catch(IOException e) { e.printStackTrace(); }
+		 */
+	}
 }
